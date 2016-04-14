b28bd04f6c19e9a635f9de06a771affd2ef9c1c7
MrRaindrop <tekkahs@gmail.com>
2016-04-15 02:38:23 +0800

    + test

--- test/index.js
+++ test/index.js
@@ -1,1 +1,16 @@
-// TODO
+import { expect } from 'chai';
+import jsdom from 'mocha-jsdom';
+
+describe('test', function () {
+    const TEST = true
+
+    jsdom()
+
+    before(function () {
+        jsdom.rerequire('../src/index')
+    })
+
+    it('should be true', function () {
+        expect(TEST).to.be.true
+    })
+})
